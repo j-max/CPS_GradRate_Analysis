63fff9cd965cbd0c5b7deeb37db1ef6228f505a6
j-max <jmax.barry@gmail.com>
2021-04-21 08:43:41 -0500

    Reduce eda.py to 1819 school year

--- src/eda.py
+++ src/eda.py
@@ -13,10 +13,26 @@ profile_1819 = pd.read_csv('../data/chicago_data_portal_csv_files/Chicago_Public
 # 95 columns
 hs_1819 = profile_1819[profile_1819['Is_High_School']==True]
 
-
+# The profile csv for 1718 need headers
 profile_1718 = pd.read_csv('../data/chicago_data_portal_csv_files/Chicago_Public_Schools_-_School_Profile_Information_SY1718.csv')
 # 92 columns
 
-profile_1617 = pd.read_csv('../data/chicago_data_portal_csv_files/Chicago_Public_Schools_-_School_Profile_Information_SY1617.csv')
-# 91 columns
+# Target column for machine learning regression models will be graduation rate
+graduation_rate = profile_1819['Graduation_Rate_School']
 
+print(f"""For all records in the 1819 school year, there are {
+                                    graduation_rate.isna().sum()
+                                    } na values""")
+
+# Since graduation rates are only applicable to high schools
+# I subset on the `Is_High_School` column.
+
+hs_1819 = profile_1819[profile_1819['Is_High_School']==True]
+
+print(f"""
+2018-19 Records
+Original number of schools: {profile_1819.shape[0]}
+Number of high schools: {hs_1819.shape[0]}
+""")
+
+########### Years prior to 1819 school year 
