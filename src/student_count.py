92169ab034c7534251b06319401bca32efd64fec
j-max <jmax.barry@gmail.com>
2021-05-10 08:08:13 -0500

    Clean up src folder and old notebooks

--- src/student_count.py
+++
@@ -1,9 +0,0 @@
-student_categories = ['student_count_total','student_count_asian',
-                'student_count_asian_pacific_islander',
-                'student_count_black','student_count_english_learners',
-                'student_count_ethnicity_not_available',
-                'student_count_hawaiian_pacific_islander', 
-                'student_count_hispanic','student_count_low_income', 
-                 'student_count_multi','student_count_native_american', 
-                 'student_count_other_ethnicity',
-                'student_count_special_ed','student_count_white']
