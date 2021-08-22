dabe453a64afd63a094fc438765054fd39be6b96
j-max <jmax.barry@gmail.com>
2021-08-22 10:03:10 -0500

    Add SchoolYear class

--- src/preprocessing/preprocessing_schoolid.py
+++ src/preprocessing/preprocessing_schoolid.py
@@ -13,253 +13,235 @@ eda = full_path.split(home_folder)[0] + home_folder + '/' + 'notebooks/eda/'
 
 sys.path.append(root)
 
-def merge_pr_and_sp(path_to_sp_csv, path_to_pr_csv):
 
-    '''
-    The two main sources of data for this project come from
-    School Profile csv and the Progress report csv.
+class SchoolYear():
 
-    This function merges the two on the School_ID column.
+    def __init__(self, path_to_sp_csv, path_to_pr_csv):
 
-    Parameters:
-    path_to_sp_csv: path to the School Profile csv
-    path_to_pr_csv: path to the Progress Report csv
+        '''
+        Initialize SchoolYear object with a path to
+        a school profile csv and a progress report csv.
+        '''
 
-    Returns:
-    merged_df: A dataframe with all of the columns from both
-    the school profile and progress report csvs
-    '''
-    sp_df = pd.read_csv(path_to_sp_csv)
-    pr_df = pd.read_csv(path_to_pr_csv)
+        self.path_to_sp_csv = path_to_sp_csv
+        self.path_to_pr_csv = path_to_pr_csv
+        self.sp_df = pd.read_csv(self.path_to_sp_csv)
+        self.pr_df = pd.read_csv(self.path_to_pr_csv)
+        self.original_merged_df = self.merge_pr_and_sp()
+        self.merged_df = self.original_merged_df.copy()
 
-    merged_df = sp_df.merge(pr_df, on='School_ID', suffixes= ('_sp', '_pr') )
+    def merge_pr_and_sp(self):
 
-    return merged_df
+        '''
+        The two main sources of data for this project come from
+        School Profile csv and the Progress report csv.
 
-def isolate_high_schools(merged_df):
+        This function merges the two on the School_ID column.
 
-    merged_df = merged_df[merged_df['Is_High_School'] == True]
+        Parameters:
+        path_to_sp_csv: path to the School Profile csv
+        path_to_pr_csv: path to the Progress Report csv
 
-    return merged_df
+        Returns:
+        merged_df: A dataframe with all of the columns from both
+        the school profile and progress report csvs
+        '''
 
+        self.original_merged_df = self.sp_df.merge(self.pr_df, on='School_ID', suffixes= ('_sp', '_pr') )
 
-def drop_no_gr_schools(merged_df):
+        # make a copy so that originals can be compared to preprocessed df
 
-    '''
-    Drop schools from the dataframe that do not have graduation rates.
-    This function is meant to be called after isolating high schools.
+        return self.original_merged_df
 
-    Many of the high schools without graduation rates are charter schools.
-    For example, YCCS (Youth Connection Charter Schools).
-    '''
+    def isolate_high_schools(self):
 
-    merged_df.dropna(subset=['Graduation_Rate_School'], inplace=True)
+        self.merged_df = self.merged_df[self.merged_df['Is_High_School'] == True]
 
-    return merged_df
- 
-def drop_specialed_options(merged_df, drop_options=True):
+        return self.merged_df
 
-    '''
-    Special Education and Citywide Options schools have different
-    missions and do not prioritize graduation rates.  They report
-    graduation rates means below 20%.
 
-    parameters:
-    merged_df: a dataframe with both school profile and progress report
-    data merged together.
+    def drop_no_gr_schools(self):
 
-    drop_options: a boolean that, if True, drops schools
-    with School-Type='Citywide-Options'
+        '''
+        Drop schools from the dataframe that do not have graduation rates.
+        This function is meant to be called after isolating high schools.
 
-    Returns:
-    merged_df: dataframe with schools that have graduation rates
-    suitable for modeling.
-    '''
+        Many of the high schools without graduation rates are charter schools.
+        For example, YCCS (Youth Connection Charter Schools).
+        '''
 
-    if drop_options:
+        self.merged_df.dropna(subset=['Graduation_Rate_School'], inplace=True)
 
-        merged_df = merged_df[~merged_df['School_Type'].isin(
-                     ['Citywide-Option', 'Special Education'])]
+        return self.merged_df
 
-        return merged_df
+    def drop_specialed_options(self, drop_options=True):
 
-    else:
+        '''
+        Special Education and Citywide Options schools have different
+        missions and do not prioritize graduation rates.  They report
+        graduation rates means below 20%.
 
-        merged_df = merged_df[~merged_df['School_Type'].isin(
-                     ['Special Education'])]
+        parameters:
+        self.merged_df: a dataframe with both school profile and progress report
+        data merged together.
 
-        return merged_df
+        drop_options: a boolean that, if True, drops schools
+        with School-Type='Citywide-Options'
 
-def make_percent_low_income(merged_df):
+        Returns:
+        self.merged_df: dataframe with schools that have graduation rates
+        suitable for modeling.
+        '''
 
-    '''
-    Create new column which is percent of the total population which is low income.
+        if drop_options:
 
-    Parameters:
-    merged_df: dataframe of school id's with low income population count.
+            self.merged_df = self.merged_df[~self.merged_df['School_Type'].isin(
+                        ['Citywide-Option', 'Special Education'])]
 
-    Returns:
-    A dataframe with a per_low_income column added to it.
-    '''
+            return self.merged_df
 
-    merged_df['perc_low_income'] = 0
+        else:
 
-    merged_df['perc_low_income'] = merged_df['Student_Count_Low_Income']/merged_df['Student_Count_Total']
+            self.merged_df = self.merged_df[~self.merged_df['School_Type'].isin(
+                        ['Special Education'])]
 
-    merged_df.fillna({'perc_low_income': 0}, inplace=True)
+            return self.merged_df
 
-    return merged_df
+    def make_percent_low_income(self):
 
-def creative_school_encoding(merged_df, drop=True, drop_column='EMERGING'):
+        '''
+        Create new column which is percent of the total population which is low income.
 
-    '''
-    The creative school certification shows a statistically significant
-    effect on graduation rates based on visual inspection and an ANOVA test.
+        Parameters:
+        self.merged_df: dataframe of school id's with low income population count.
 
-    This function One Hot Encodes the 'Creative_School_Certification' column.
+        Returns:
+        A dataframe with a per_low_income column added to it.
+        '''
 
-    Parameters:
-    merged_df: dataframe of school id's with Creative_School_Certification column.
+        self.merged_df['perc_low_income'] = 0
 
-    drop: parameter to designate dropping a column.  To be set to true with
-    vanilla linear regression models.
+        self.merged_df['perc_low_income'] = self.merged_df['Student_Count_Low_Income']/self.merged_df['Student_Count_Total']
 
-    drop_column:  The column to drop.  Set by default to 'EMERGING' so that the
-    regression coefficients will be positive in relation to the lowest certificate
-    achievement category.
+        self.merged_df.fillna({'perc_low_income': 0}, inplace=True)
 
-    Returns:
-    Dataframe with a one_hot_encoded version of Creative_School_Certification
-    and the original column dropped.
+        return self.merged_df
 
-    '''
+    def creative_school_encoding(self, drop=True, drop_column='EMERGING'):
 
-    if drop:
-        ohe_csc = OneHotEncoder(drop=['EMERGING'], sparse=False )
+        '''
+        The creative school certification shows a statistically significant
+        effect on graduation rates based on visual inspection and an ANOVA test.
 
-        csc_ohe = pd.DataFrame(
-                            ohe_csc.fit_transform(
-                                merged_df[['Creative_School_Certification']]), 
-                            index=merged_df.index, 
-                            columns = ohe_csc.get_feature_names(['creative']))
+        This function One Hot Encodes the 'Creative_School_Certification' column.
 
-        merged_df = merged_df.merge(csc_ohe, left_index=True, right_index=True)
-        merged_df.drop('Creative_School_Certification', axis=1, inplace=True)
+        Parameters:
+        self.merged_df: dataframe of school id's with Creative_School_Certification column.
 
-        return merged_df
+        drop: parameter to designate dropping a column.  To be set to true with
+        vanilla linear regression models.
 
+        drop_column:  The column to drop.  Set by default to 'EMERGING' so that the
+        regression coefficients will be positive in relation to the lowest certificate
+        achievement category.
 
-def prep_teacher_attendance(merged_df):
+        Returns:
+        Dataframe with a one_hot_encoded version of Creative_School_Certification
+        and the original column dropped.
 
-    '''
-    Teacher attendance is recorded for two years in the progress report.
-    For example, school year 18-19 will have teacher attendance rates
-    for 2018 and 2019.
+        '''
 
-    This function takes the average across the two years.
-    '''
+        if drop:
+            ohe_csc = OneHotEncoder(drop=['EMERGING'], sparse=False )
 
-    merged_df['teacher_attendance'] = (merged_df['Teacher_Attendance_Year_1_Pct'] +\
-                                       merged_df['Teacher_Attendance_Year_2_Pct'])/2
+            csc_ohe = pd.DataFrame(
+                                ohe_csc.fit_transform(
+                                    self.merged_df[['Creative_School_Certification']]), 
+                                index=self.merged_df.index, 
+                                columns = ohe_csc.get_feature_names(['creative']))
 
- 
-    return merged_df
+            self.merged_df = self.merged_df.merge(csc_ohe, left_index=True, right_index=True)
+            self.merged_df.drop('Creative_School_Certification', axis=1, inplace=True)
 
-def create_df_for_modeling(merged_df):
+            return self.merged_df
 
-    df = isolate_high_schools(merged_df)
-    df = drop_no_gr_schools(df)
-    df = drop_specialed_options(df)
-    df = make_percent_low_income(df)
-    df = creative_school_encoding(df)
-    df = prep_teacher_attendance(df)
 
-    return df
+    def prep_teacher_attendance(self):
 
+        '''
+        Teacher attendance is recorded for two years in the progress report.
+        For example, school year 18-19 will have teacher attendance rates
+        for 2018 and 2019.
 
-def isolate_important_columns(sy_df):
+        This function takes the average across the two years.
+        '''
 
-    # These columns are consistent across 2016-2019 School Years
-    sy_df = sy_df[[ "School_ID","Short_Name","Graduation_Rate_School",
-                    "Student_Count_Total", "Student_Count_Low_Income",
-                    "Student_Count_Special_Ed","Student_Count_English_Learners",
-                    "Student_Count_Black","Student_Count_Hispanic",
-                    "Student_Count_White", "Student_Count_Asian",
-                    "Student_Count_Native_American","Student_Count_Other_Ethnicity",
-                    "Student_Count_Asian_Pacific_Islander","Student_Count_Multi",
-                    "Student_Count_Hawaiian_Pacific_Islander",
-                    "Student_Count_Ethnicity_Not_Available", 'Is_High_School', 'Dress_Code',
-                    "Classroom_Languages","Transportation_El"]]
+        self.merged_df['teacher_attendance'] = (self.merged_df['Teacher_Attendance_Year_1_Pct'] +\
+                                        self.merged_df['Teacher_Attendance_Year_2_Pct'])/2
 
-    return sy_df
 
-def isolate_important_columns_pr_rep(pr_df):
+        return self.merged_df
 
-    pr_df = pr_df[["School_ID", "School_Type"]]
+    def isolate_important_columns(sy_df):
 
-    return pr_df
+        # These columns are consistent across 2016-2019 School Years
+        sy_df = sy_df[[ "School_ID","Short_Name","Graduation_Rate_School",
+                        "Student_Count_Total", "Student_Count_Low_Income",
+                        "Student_Count_Special_Ed","Student_Count_English_Learners",
+                        "Student_Count_Black","Student_Count_Hispanic",
+                        "Student_Count_White", "Student_Count_Asian",
+                        "Student_Count_Native_American","Student_Count_Other_Ethnicity",
+                        "Student_Count_Asian_Pacific_Islander","Student_Count_Multi",
+                        "Student_Count_Hawaiian_Pacific_Islander",
+                        "Student_Count_Ethnicity_Not_Available", 'Is_High_School', 'Dress_Code',
+                        "Classroom_Languages","Transportation_El"]]
 
-def convert_is_high_school_to_bool(sy_df):
-    '''
-    Some of the School Year profiles' Is_High_School columns are encoded as booleans,
-    df some as Y/N.  This function encodes them all as booleans.
-    '''
+        return sy_df
 
-    if sy_df['Is_High_School'].dtype == 'O':
-        # Convert y/n to True/False
-        sy_df['Is_High_School'] = sy_df['Is_High_School'].map({'Y':True, 'N':False})
+    def isolate_important_columns_pr_rep(pr_df):
 
-    return sy_df
+        pr_df = pr_df[["School_ID", "School_Type"]]
 
-def remove_no_student_count_schools(sy_df):
+        return pr_df
 
-    '''
-    If a school does not have any students recorded in the school year profile, remove the school.
-    '''
+    def convert_is_high_school_to_bool(self):
+        '''
+        Some of the School Year profiles' Is_High_School columns are encoded as booleans,
+        df some as Y/N.  This function encodes them all as booleans.
+        '''
 
-    sy_df = sy_df[sy_df['Student_Count_Total'] > 0]
+        if self.merged_df['Is_High_School'].dtype == 'O':
+            # Convert y/n to True/False
+            self.sy_df['Is_High_School'] = self.sy_df['Is_High_School'].map({'Y':True, 'N':False})
 
-    return sy_df 
+        return self.sy_df
 
-def load_prepped_school_year_profile(path_to_school_year_profile):
+    def remove_no_student_count_schools(self):
 
-    '''
-    There are 660 schools represented in the CPS Data Portal data files.
+        '''
+        If a school does not have any students recorded in the school year profile, remove the school.
+        '''
 
-    141 of these are high schools. 
+        self.merged_df = self.merged_df[self.merged_df['Student_Count_Total'] > 0]
 
-    '''
-    df = pd.read_csv(path_to_school_year_profile)
+        return self.merged_df 
 
-    df = isolate_important_columns(df)
-    df = convert_is_high_school_to_bool(df)
-    df = isolate_high_schools(df)
-    df = drop_no_gr_schools(df)
-    df = make_percent_low_income(df)
-    df = remove_no_student_count_schools(df)
+    def create_df_for_modeling(self):
 
-    return df
+        self.isolate_high_schools()
+        self.drop_no_gr_schools()
+        self.drop_specialed_options()
+        self.make_percent_low_income()
+        self.creative_school_encoding()
+        self.prep_teacher_attendance()
+        self.remove_no_student_count_schools()
 
-def merge_prepped_sy_profile_and_prog_rep(path_to_school_year_profile,
-                                           path_to_progress_report):
+        return self.merged_df 
 
-    df_sy = load_prepped_school_year_profile(path_to_school_year_profile)
 
-    df_pr = pd.read_csv(path_to_progress_report)
-    df_pr = isolate_important_columns_pr_rep(df_pr)
-    df_merged = df_sy.merge(df_pr, on='School_ID')
 
+### Tests
 
-    return df_merged
+sy_1819 = SchoolYear('../../data/chicago_data_portal_csv_files/Chicago_Public_Schools_-_School_Profile_Information_SY1819.csv', '../../data/chicago_data_portal_csv_files/.ipynb_checkpoints/Chicago_Public_Schools_-_School_Progress_Reports_SY1819-checkpoint.csv')
 
-
-def load_unprocessed_hs_df(path_to_school_year_profile):
-
-    df = pd.read_csv(path_to_school_year_profile)
-
-    df = convert_is_high_school_to_bool(df)
-    df = isolate_high_schools(df)
-
-    return df
-
-
+df_1819 = sy_1819.create_df_for_modeling()
