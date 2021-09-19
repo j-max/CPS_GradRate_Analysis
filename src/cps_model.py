8e0b560e2a3e49fb174dd871e95add1264e0296a
j-max <jmax.barry@gmail.com>
2021-09-19 11:07:23 -0500

    Add print_cs_results function

--- src/cps_model.py
+++ src/cps_model.py
@@ -1,5 +1,6 @@
 import sklearn.pipeline
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import cross_val_predict, cross_validate
+from sklearn.metrics import mean_squared_error, r2_score
 
 def cv_cps(model, X_train, y_train, features):
 
@@ -26,3 +27,44 @@ def cv_cps(model, X_train, y_train, features):
         print('Coefficients from fit on full train set')
         print(model.coef_, model.intercept_)
 
+
+
+
+def print_cv_results(model, X_train, features_list, y_train):
+    
+    cv_fsm = cross_validate(model, X_train[features_list], y_train, cv=3, 
+                        return_train_score=True, scoring=['neg_root_mean_squared_error', 'r2'])
+
+    
+    print('#####r2#####')
+
+    print(cv_fsm['train_r2'].mean())
+    print('test')
+    print(cv_fsm['test_r2'])
+    print(cv_fsm['test_r2'].mean())
+    print('#####rmse#####')
+    print("#####Train####")
+    print(-cv_fsm['train_neg_root_mean_squared_error'])
+    print(-cv_fsm['train_neg_root_mean_squared_error'].mean())
+    print('test')
+    print(-cv_fsm['test_neg_root_mean_squared_error'])
+    print(-cv_fsm['test_neg_root_mean_squared_error'].mean())
+    
+    
+    # Cross Val Predict
+    print('cross val predict metrics: test scores')
+    y_hat = cross_val_predict(model, X_train[features_list], y_train, cv=3)
+    
+    rmse = mean_squared_error(y_train, y_hat, squared=False)
+    '''As stated in the docs, these numbers are a bit different 
+    most likely because of different test sizes in each fold'''
+    
+    print("RMSE")
+    print(rmse)
+    
+    r2 = r2_score(y_train, y_hat)
+    print("r2")
+
+    print(r2)
+    
+    return cross_val_predict(model, X_train[features_list], y_train, cv=3)
