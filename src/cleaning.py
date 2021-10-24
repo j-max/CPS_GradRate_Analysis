1b265e9716ff5d0910e8ec8ac2f75b2eb8a77c2b
j-max <jmax.barry@gmail.com>
2021-10-24 11:10:11 -0500

    Add drop no student/grad rates definitions to the cleaning file

--- src/cleaning.py
+++ src/cleaning.py
@@ -14,15 +14,17 @@ eda = full_path.split(home_folder)[0] + home_folder + '/' + 'notebooks/eda/'
 sys.path.append(root)
 
 
-
 def import_and_merge_data(path_to_sp_csv, path_to_pr_csv):
 
-        sp_df = pd.read_csv(path_to_sp_csv)
-        pr_df = pd.read_csv(path_to_pr_csv)
+    'Simple merge of two school csv files'
 
-        merged_df = sp_df.merge(pr_df, on='School_ID', suffixes= ('_sp', '_pr') )
+    sp_df = pd.read_csv(path_to_sp_csv)
+    pr_df = pd.read_csv(path_to_pr_csv)
 
-        return merged_df
+    merged_df = sp_df.merge(pr_df, on='School_ID', suffixes= ('_sp', '_pr') )
+
+    return merged_df
+
 
 def drop_no_students(merged_df):
 
@@ -43,6 +45,7 @@ def drop_no_students(merged_df):
 
     return merged_df
 
+
 def drop_no_grad_rate(merged_df):
 
     ''' 
@@ -60,4 +63,66 @@ def drop_no_grad_rate(merged_df):
         print('All 0 Graduation Rate Schools Dropped')
 
     return merged_df
+
+
+
+def convert_is_high_school_to_bool(merged_df):
+
+    '''
+    Some of the School Year profiles' Is_High_School columns are encoded as booleans,
+    df some as Y/N.  This function encodes them all as booleans.
+    '''
+
+    if merged_df['Is_High_School'].dtype == 'O':
+        # Convert y/n to True/False
+        merged_df['Is_High_School'] = merged_df['Is_High_School'].map({'Y':True, 'N':False})
+
+    return merged_df
+
+def make_percent_low_income(merged_df):
+
+    '''
+    Create new column which is percent of the total population which is low income.
+
+    Parameters:
+    self.merged_df: dataframe of school id's with low income population count.
+
+    Returns:
+    A dataframe with a per_low_income column added to it.
+    '''
+
+    merged_df['perc_low_income'] = 0
+
+    merged_df['perc_low_income'] = merged_df['Student_Count_Low_Income']/merged_df['Student_Count_Total']
+
+    merged_df.fillna({'perc_low_income': 0}, inplace=True)
+
+    return merged_df
+
+
+def isolate_high_schools(merged_df):
+
+    merged_df = merged_df[merged_df['Is_High_School'] == True]
+
+    return merged_df
+
+
+def isolate_important_columns(merged_df):
+
+    # These columns are consistent across 2016-2019 School Years
+    sy_important_columns = [ "School_ID","Short_Name","Graduation_Rate_School",
+                    "Student_Count_Total", "Student_Count_Low_Income",
+                    "Student_Count_Special_Ed","Student_Count_English_Learners",
+                    "Student_Count_Black","Student_Count_Hispanic",
+                    "Student_Count_White", "Student_Count_Asian",
+                    "Student_Count_Native_American","Student_Count_Other_Ethnicity",
+                    "Student_Count_Asian_Pacific_Islander","Student_Count_Multi",
+                    "Student_Count_Hawaiian_Pacific_Islander",
+                    "Student_Count_Ethnicity_Not_Available", 'Is_High_School', 'Dress_Code',
+                    "Classroom_Languages","Transportation_El"]
+
+
+    pr_important_columns = ["School_ID", "School_Type"]
+
+    return merged_df[sy_df_important_columns + pr_important_columns]
 
